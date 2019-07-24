524a0a5ac3e2f4beef5aba3a7b19ae35ea5146e6
Shieh, Jonathan (jx789v) <jx789v@att.com>
2019-07-24 00:40:56 -0500

    Add Card layout and models and initial rendering of your events page

---
+++ ospreymobile/src/model/WatchParty.js
@@ -0,0 +1,10 @@
+export class WatchParty {
+
+    constructor(show, id, date, time, hostId) {
+        this.id = id;
+        this.show = show;
+        this.date = date;
+        this.time = time;
+        this.hostId = hostId;
+    }
+}
