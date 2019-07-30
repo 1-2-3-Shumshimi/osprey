4be0fc8a804644781a34006dc4f06dd1dde7dbcc
Beinart, Jacob (jz043x) <jz043x@us.att.com>
2019-07-30 08:52:50 -0500

    Fixing navigation to create party page by providing a dummy show

--- ospreymobile/pages/BrowseDetails.js
+++ ospreymobile/pages/BrowseDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {  View,  Text, Button } from 'react-native';
+import { Show } from '../src/model/Show';
 
 export default class BrowseDetails extends Component {
     render() {
@@ -9,7 +10,17 @@ export default class BrowseDetails extends Component {
           <Button
             title="Set Up a Watch Party"
             onPress={() => 
-              this.props.navigation.navigate('CreateWatchParty')
+              this.props.navigation.navigate('CreateWatchParty',
+              {
+                show: new Show(
+                  2,
+                  `A high school chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine in order to secure his family's future.`,
+                  'Crime, Drama, Thriller',
+                  'https://img.reelgood.com/content/show/f75762df-3e5b-4cd3-b621-32399a3dd20d/poster-342.jpg',
+                  'Breaking Bad',
+                  '2008'
+                ),
+              })
             }
           />
         </View>
